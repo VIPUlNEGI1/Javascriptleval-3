4593970cf8f77619e6e2999def3edc29ef75f946
vipul negi <167672450+VIPUlNEGI1@users.noreply.github.com>
2024-10-13 16:01:19 +0000

    hi

---
+++ excutioncontext.js
@@ -0,0 +1,17 @@
+// what is excautionContext
+
+function SetUsername(username){
+
+    this.username = username
+    console.log('called')
+}
+
+function createUser ( username , email, password){
+    SetUsername.call(this, username)
+    this.email = email
+    this.password = password
+// this.username = username
+
+}
+const chai = new createUser('chai','chai@gmail.com','12345y')
+console.log(chai)
