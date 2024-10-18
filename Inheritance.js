6c6b0080db3afbb563e71839cac9235afca33f03
vipul negi <167672450+VIPUlNEGI1@users.noreply.github.com>
2024-10-18 18:53:15 +0000

    hi

--- Inheritance.js
+++ Inheritance.js
@@ -1,1 +1,29 @@
-class
+class user {
+    constructor(username){
+        this.username= username
+    }
+    logMe(){
+        console.log(`USERNAME is ${this.username}`)
+    }
+}
+class Teacher extends user{
+    constructor(username,email, password){
+//    this (super ) keyword is very spical
+        super(username)
+        this.email= email
+       this.password= password
+    }
+ addCorses(){
+    console.log(`A new corse was added by ${this.username}`)
+ }
+}
+const chai = new Teacher("chai","chai@teacher.com", "123")
+chai.addCorses()
+
+const masalaCHai = new user("masalaChai")
+masalaCHai.logMe()
+
+
+console.log(chai === masalaCHai)
+
+console.log(chai instanceof user)
