410ce6bdd16e81db30cbfcb551d94b3dd756d81c
vipul negi <167672450+VIPUlNEGI1@users.noreply.github.com>
2024-10-18 18:16:09 +0000

    new

--- excutioncontext.js
+++ excutioncontext.js
@@ -7,7 +7,7 @@ function SetUsername(username){
 }
 
 function createUser ( username , email, password){
-    SetUsername.call(this, username)
+    SetUsername.call(this, username).
     this.email = email
     this.password = password
 // this.username = username
